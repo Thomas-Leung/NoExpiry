234647f7e904eb6945930510a0f2e2f692238794
thomas <thomaselmoleung@gmail.com>
2019-11-03 23:44:31 -0500

    UI for App and Login page

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,34 @@
   <!-- With the key, any change to the path will trigger a reload of the component with the new data. :key="$route.fullPath" -->
   <v-app>
     <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <router-link to="/">
-          <span class="font-weight-regular">No Expiry</span>
-        </router-link>
+      <v-toolbar-title class="headline">
+        <router-link to="/">No Expiry</router-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <div v-if="isLoggedIn">{{currentUser}}</div>
-      <div v-if="isLoggedIn">
-        <router-link to="/">Dashboard</router-link>
-      </div>
-      <div v-if="!isLoggedIn">
-        <router-link to="/login">Login</router-link>
-      </div>
-      <div v-if="!isLoggedIn">
-        <router-link to="/register">Register</router-link>
-      </div>
-      <div v-if="isLoggedIn">
-        <button v-on:click="logout">Logout</button>
-      </div>
-      <v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+      <v-toolbar-items>
+        <v-btn lower class="text-none" text v-if="isLoggedIn">{{currentUser}}</v-btn>
+        <v-btn text class="text-none" v-if="isLoggedIn">
+          <router-link to="/">Dashboard</router-link>
+        </v-btn>
+        <v-btn text class="text-none" v-if="!isLoggedIn">
+          <router-link to="/login">Login</router-link>
+        </v-btn>
+        <v-btn text class="text-none" v-if="!isLoggedIn">
+          <router-link to="/register">Register</router-link>
+        </v-btn>
+        <v-btn text class="text-none" v-if="isLoggedIn" v-on:click="logout">Logout</v-btn>
+      </v-toolbar-items>
     </v-app-bar>
 
     <v-content>
-      <router-link to="/new">
-        <v-btn color="blue darken-2" absolute dark fab bottom right style="margin-bottom:50px">
+      <router-link to="/new" v-if="isLoggedIn">
+        <v-btn
+          color="blue-grey lighten-4"
+          absolute
+          fab
+          right
+          style="bottom: 0; position: absolute; margin: 0 0 16px 16px;"
+        >
           <v-icon>mdi-plus</v-icon>
         </v-btn>
       </router-link>
@@ -72,22 +73,12 @@ export default {
 <style>
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+/* Remove router link underline */
+a {
+  text-decoration: none;
+  color: black !important;
 }
 </style>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,25 +1,47 @@
 <template>
   <div class="container">
-    <h3>Login</h3>
-    <form>
-      <input type="text" id="email" v-model="email"/>
-      <label for="email">Email</label>
-      <input type="password" id="password" v-model="password">
-      <label for="password">Password</label>
-      <button @click="login">Login</button>
-    </form>
+    <v-card class="mx-auto" max-width="344" shaped>
+      <v-list-item three-line>
+        <v-list-item-content>
+          <v-list-item-title class="headline mb-10">Login</v-list-item-title>
+          <v-text-field label="Email" outlined v-model="email"></v-text-field>
+          <v-text-field
+            v-model="password"
+            :append-icon="showPwd ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="[rules.required, rules.min]"
+            :type="showPwd ? 'text' : 'password'"
+            name="input-password"
+            label="Password"
+            hint="At least 6 characters"
+            counter
+            outlined
+            @click:append="showPwd = !showPwd"
+          ></v-text-field>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn text @click="login">Login</v-btn>
+      </v-card-actions>
+    </v-card>
   </div>
 </template>
 
 <script>
-import firebase from 'firebase'
+import firebase from "firebase";
 
 export default {
   name: "login",
-   data: function() {
+  data: function() {
     return {
       email: "",
-      password: ""
+      password: "",
+      showPwd: false,
+      rules: {
+        required: value => !!value || "Required.",
+        min: v => v.length >= 6 || "Min 6 characters"
+      }
     };
   },
   methods: {
@@ -31,7 +53,7 @@ export default {
           user => {
             alert(`You are logged in as ${user.user.email}`);
             //reload the whole page
-            this.$router.go({path: this.$router.replace('/')});
+            this.$router.go({ path: this.$router.replace("/") });
           },
           err => {
             alert(err.message);
