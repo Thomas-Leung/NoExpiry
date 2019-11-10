773fa94c7f76c263311d43f7595f2e2252687dbe
thomas <thomaselmoleung@gmail.com>
2019-11-10 14:03:40 -0500

    add UI for dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,5 +1,60 @@
 <template>
   <div id="dashboard">
+    <v-toolbar dense floating class="searchBar">
+      <v-text-field hide-details prepend-icon="mdi-magnify" single-line></v-text-field>
+
+      <v-menu bottom left :close-on-content-click="true" :nudge-width="300" offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-card>
+          <v-list>
+            <v-list-item>
+              <v-list-item-avatar>
+                <v-icon>mdi-account-card-details</v-icon>
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <v-list-item-title class="userEmail">{{currentUser}}</v-list-item-title>
+              </v-list-item-content>
+
+              <v-list-item-action>
+                <v-btn
+                  :class="darkMode? 'white--text' : 'grey--text'"
+                  icon
+                  @click="darkMode = !darkMode"
+                >
+                  <v-icon>mdi-brightness-6</v-icon>
+                </v-btn>
+              </v-list-item-action>
+            </v-list-item>
+          </v-list>
+          <v-divider></v-divider>
+
+          <v-list>
+            <v-list-item-group>
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon style="padding-left:4px;">mdi-basket</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title>Groceries list</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-btn text class="red--text caption">Logout</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-menu>
+    </v-toolbar>
+
     <h3>Items</h3>
     <ul>
       <li v-for="item in items" :key="item.id">
@@ -19,7 +74,9 @@ export default {
   data() {
     return {
       items: [],
-      receive: false
+      receive: false,
+      currentUser: false,
+      darkMode: false
     };
   },
   created() {
@@ -51,9 +108,20 @@ export default {
           this.items.push(data);
         });
       });
+
+    if (firebase.auth().currentUser) {
+      this.currentUser = firebase.auth().currentUser.email;
+    }
   }
 };
 </script>
 
 <style>
+.searchBar.v-toolbar {
+  border-radius: 10px;
+}
+
+.v-list-item__title {
+  text-align: start;
+}
 </style>
