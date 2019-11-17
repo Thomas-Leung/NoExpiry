9a2f415d2d0716962b7650407fd9285b33ef3c06
thomas <thomaselmoleung@gmail.com>
2019-11-17 00:17:09 -0500

    add card UI in dashboard view

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,7 +1,65 @@
 <template>
   <div id="dashboard">
     <Searchbar />
-    <h3>Items</h3>
+    <p class="headline font-weight-bold text-justify pl-6">Your Food</p>
+    <div class="swipe-content">
+      <swipe-list
+        class="card"
+        :disabled="!enabled"
+        :items="items"
+        item-key="item.id"
+      >
+        <template v-slot="{ item, index, revealLeft, revealRight, close }">
+          <!-- item is the corresponding object from the array -->
+          <!-- index is clearly the index @swipeout:click="itemClick"-->
+          <!-- revealLeft is method which toggles the left side -->
+          <!-- revealRight is method which toggles the right side -->
+          <!-- close is method which closes an opened side -->
+          <div class="card-content">
+            <table style="width:100%; border-spacing: 0px;">
+              <tr>
+                <td class="title" style="text-align:start; padding: 10px 0px 0px 16px;">{{ item.name }}</td>
+                <td class="body-2 font-weight-light" rowspan="2">
+                  Amount
+                  <br />
+                  {{ item.amount }}
+                </td>
+                <td style="background-color:#CFD8DC; border-radius:0px 20px 0px 0px">
+                  <v-icon dark>mdi-chevron-up</v-icon>
+                </td>
+              </tr>
+              <tr>
+                <td
+                  class="subtitle-2 font-weight-light"
+                  style="text-align:start; padding: 0px 0px 10px 16px;"
+                >Expiry: {{ item.dateExpiry }}</td>
+                <td style="background-color:#CFD8DC; border-radius:0px 0px 20px 0px" @click="itemClick">
+                  <v-icon dark>mdi-chevron-down</v-icon>
+                </td>
+              </tr>
+            </table>
+            <p></p>
+          </div>
+        </template>
+        <template v-slot:left="{ item, close }">
+          <div class="swipeout-action grey">
+            <v-icon dark>mdi-eye-off</v-icon>
+          </div>
+          <div class="swipeout-action dark-blue" @click="close">
+            <v-icon dark>mdi-pencil</v-icon>
+          </div>
+        </template>
+        <template v-slot:right="{ item }">
+          <div class="swipeout-action dark-red" @click="remove(item)">
+            <v-icon dark>mdi-delete</v-icon>
+          </div>
+        </template>
+      </swipe-list>
+      <div v-if="this.mockSwipeList.length === 0">
+        <p class="body-1"><v-icon>mdi-noodles</v-icon> No Item</p>
+      </div>
+    </div>
+
     <ul>
       <li v-for="item in items" :key="item.id">
         Name: {{item.name}}
@@ -25,17 +83,34 @@
 import firebase from "firebase";
 import db from "../components/firebaseInit";
 import Searchbar from "@/components/Searchbar.vue";
+import { SwipeList } from "vue-swipe-actions";
 
 export default {
   name: "dashboard",
   components: {
-    Searchbar
+    Searchbar,
+    SwipeList
   },
   data() {
     return {
       items: [],
       receive: false,
-      currentUser: false
+      currentUser: false,
+      enabled: true,
+      mockSwipeList: [
+        {
+          id: 0,
+          name: "Orange",
+          amount: 1,
+          dateExpiry: "2019-11-13"
+        },
+        {
+          id: 1,
+          name: "Apple",
+          amount: 1,
+          dateExpiry: "2019-11-13"
+        }
+      ]
     };
   },
   created() {
@@ -80,13 +155,74 @@ export default {
       const date = new Date(seconds * 1000);
       // Janurary gives 0
       return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+    },
+    remove(item) {
+      this.mockSwipeList = this.mockSwipeList.filter(i => i !== item);
+      // console.log(e, 'remove');
+    },
+    itemClick(e) {
+      console.log(e, "item click");
     }
   }
 };
 </script>
 
-<style scoped>
-.v-list-item__title {
-  text-align: start;
+<style>
+@import "../../node_modules/vue-swipe-actions/dist/vue-swipe-actions.css";
+
+.swipe-content {
+  /* top (right,left) botton */
+  padding: 0px 20px 20px;
+}
+.card {
+  width: 100%;
+  border-radius: 0px;
+  box-shadow: none;
+  /* border: 1px solid lightgray; */
+}
+
+.card-content {
+  padding: 0rem;
+  background: #99cd49;
+  border-radius: 20px;
+  color: white;
+}
+
+.swipeout-action {
+  display: flex;
+  align-items: center;
+  padding: 0 3rem;
+  cursor: pointer;
+  left: 0;
+}
+
+.swipeout-action.grey {
+  background-color: rgb(199, 198, 204);
+  margin-bottom: 16px;
+  border-radius: 10px;
+  padding: 30px;
+}
+
+.swipeout-action.dark-blue {
+  background-color: rgb(74, 111, 163);
+  margin-bottom: 16px;
+  border-radius: 10px;
+  padding: 30px;
+}
+
+.swipeout-action.dark-red {
+  background-color: #d32f2f;
+  margin-bottom: 16px;
+  border-radius: 10px;
+  padding: 30px;
+}
+
+.swipeout-list-item {
+  flex: 1;
+  /* border-bottom: 1px solid lightgray; */
+}
+
+.swipeout-list-item:last-of-type {
+  border-bottom: none;
 }
 </style>
--- src/views/Groceries.vue
+++ src/views/Groceries.vue
@@ -1,131 +1,19 @@
 <template>
   <div class>
     <h1>Groceries page</h1>
-    <div class="swipe-content">
-      <swipe-list
-        class="card"
-        :disabled="!enabled"
-        :items="mockSwipeList"
-        item-key="id"
-        @swipeout:click="itemClick"
-      >
-        <template v-slot="{ item, index, revealLeft, revealRight, close }">
-          <!-- item is the corresponding object from the array -->
-          <!-- index is clearly the index -->
-          <!-- revealLeft is method which toggles the left side -->
-          <!-- revealRight is method which toggles the right side -->
-          <!-- close is method which closes an opened side -->
-          <div class="card-content">
-            <!-- style content how ever you like -->
-            <h2>{{ item.name }}</h2>
-            <p>{{ item.amount }}</p>
-            <p>{{ item.dateExpiry }}</p>
-          </div>
-        </template>
-        <template v-slot:left="{ item, close }">
-          <div class="swipeout-action blue">
-            <v-icon dark>mdi-card-search-outline</v-icon>
-          </div>
-          <div class="swipeout-action purple" @click="close">
-            <v-icon dark>mdi-pencil</v-icon>
-          </div>
-        </template>
-        <template v-slot:right="{ item }">
-          <div class="swipeout-action red" @click="remove(item)">
-            <v-icon dark>mdi-delete-outline</v-icon>
-          </div>
-        </template>
-      </swipe-list>
-      <div v-if="this.mockSwipeList.length === 0">No Item</div>
-    </div>
   </div>
 </template>
 
 <script>
-import { SwipeList } from "vue-swipe-actions";
-
 export default {
-  components: {
-    SwipeList
-  },
+  components: {},
   data() {
-    return {
-      enabled: true,
-      mockSwipeList: [
-        {
-          id: 0,
-          name: "Orange",
-          amount: 1,
-          dateExpiry: "2019-11-13"
-        },
-        {
-          id: 1,
-          name: "Apple",
-          amount: 1,
-          dateExpiry: "2019-11-13"
-        }
-      ]
-    };
+    return {};
   },
   methods: {
-    remove(item) {
-      this.mockSwipeList = this.mockSwipeList.filter(i => i !== item);
-      // console.log(e, 'remove');
-    },
-    itemClick(e) {
-      console.log(e, "item click");
-    }
   }
 };
 </script>
 
 <style>
-@import "../../node_modules/vue-swipe-actions/dist/vue-swipe-actions.css";
-
-.swipeout-action {
-  display: flex;
-  align-items: center;
-  padding: 0 3rem;
-  cursor: pointer;
-  left: 0;
-}
-
-.swipeout-action.blue {
-  color: white;
-  background-color: rgb(0, 122, 255);
-}
-.swipeout-action.blue:hover {
-  background-color: darken(rgb(0, 122, 255), 5%);
-}
-.swipeout-action.purple {
-  color: white;
-  background-color: rgb(88, 86, 214);
-}
-.swipeout-action.purple:hover {
-  background-color: darken(rgb(88, 86, 214), 5%);
-}
-.swipeout-action.red {
-  color: white;
-  background-color: rgb(255, 59, 48);
-}
-.swipeout-action.red:hover {
-  background-color: darken(rgb(255, 59, 48), 5%);
-}
-.swipeout-list-item {
-  flex: 1;
-  border-bottom: 1px solid lightgray;
-}
-.swipeout-list-item:last-of-type {
-  border-bottom: none;
-}
-.card {
-  width: 100%;
-  background-color: white;
-  border-radius: 3px;
-  box-shadow: none;
-  border: 1px solid lightgray;
-}
-.card-content {
-  padding: 1rem;
-}
 </style>
